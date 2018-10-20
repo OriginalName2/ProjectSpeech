2c84fe7a7d55e56cf61270646bca2436ba2e2bd9
Joseph Lee <josephlee.leese@gmail.com>
2018-10-20 12:45:12 -0700

    1st commit
    Uploading website code online

---
+++ ProjectSpeech/css/default.css
@@ -0,0 +1,175 @@
+@charset "utf-8";
+
+/*-----------Logo-----------*/
+
+.logo {
+    background-image: url(../images/logo.png);
+}
+
+
+/*-----------Mini Nav-----------*/
+
+.mininav a:hover {
+    color: #fff;
+    background-color: #80060a;
+}
+
+
+/*-----------Button Styles-----------*/
+
+.highlight.button {
+    color: #fff;
+    filter: progid: DXImageTransform.Microsoft.gradient(startColorstr='#80060a', endColorstr='#000000');
+    /* for IE */
+    background: -webkit-gradient(linear, left top, left bottom, from(#80060a), to(#000000));
+    /* for webkit browsers */
+    background: -moz-linear-gradient(top, #80060a, #000000);
+    /* for firefox 3.6+ */
+    border-top: 1px #b63c40 solid;
+    border-bottom: 1px #280305 solid;
+    border-right: 1px #400608 solid;
+    border-left: 1px #400608 solid;
+    text-shadow: 1px 1px 0px #111;
+}
+
+.highlight.button:hover {
+    color: #fff;
+    border-top: 1px #6b0d11 solid;
+    filter: progid: DXImageTransform.Microsoft.gradient(startColorstr='#000000', endColorstr='#80060a');
+    /* for IE */
+    background: -webkit-gradient(linear, left top, left bottom, from(#000000), to(#80060a));
+    /* for webkit browsers */
+    background: -moz-linear-gradient(top, #000000, #80060a);
+    /* for firefox 3.6+ */
+}
+
+
+/*-----------Headings-----------*/
+
+h1 {
+    color: #80060a;
+}
+
+h2 {
+    color: #80060a;
+}
+
+h3 {
+    color: #80060a;
+}
+
+
+/*-----------Sidebar Styling-----------*/
+
+.sidebar h2 {
+    color: #80060a;
+}
+
+
+/*-----------Subpage Text Styling-----------*/
+
+.subpagetext ul li:last-child {
+    color: #80060a;
+}
+
+.backgroundgradient {
+    background: -webkit-gradient(radial, right, left, from(#9c0000), to(#000000));
+    /* for webkit browsers */
+    background: -moz-radial-gradient(right, #9c0000, #000000);
+    /* for firefox 3.6+ */
+}
+
+
+/*-----------Drop Menu-----------*/
+
+#dropmenu {
+    filter: progid: DXImageTransform.Microsoft.gradient(startColorstr='#80060a', endColorstr='#000000');
+    /* for IE */
+    background: -webkit-gradient(linear, left top, left bottom, from(#80060a), to(#000000));
+    /* for webkit browsers */
+    background: -moz-linear-gradient(top, #80060a, #000000);
+    /* for firefox 3.6+ */
+    border-top: 1px #b63c40 solid;
+    border-bottom: 1px #280305 solid;
+    border-right: 1px #400608 solid;
+    border-left: 1px #400608 solid;
+}
+
+ul#dropmenu a {
+    color: #fff;
+}
+
+ul#dropmenu a:hover {
+    color: #fff;
+}
+
+ul#dropmenu a span {
+    padding-left: 25px;
+    margin-left: 5px;
+    background-image: url(../images/droparrow.png);
+    background-repeat: no-repeat;
+}
+
+
+/*-----------Misc Settings-----------*/
+
+.tip {
+    color: #fff;
+    background: #111;
+}
+
+
+/*-----------Twitter-----------*/
+
+ul#twitter_update_list li a {
+    color: #e00d14;
+}
+
+
+/*-----------Crumb Navigation-----------*/
+
+.crumb_navigation ul a:hover {
+    color: #fff;
+    background-color: #80060a;
+}
+
+
+/*-----------Nivo Slider Caption-----------*/
+
+.nivo-caption p {
+    color: #e00d14;
+}
+
+
+/*-----------Galleria Slider Captions-----------*/
+
+.galleria-info-title {
+    color: #e00d14;
+}
+
+.galleria-info-description {
+    color: #fff;
+}
+
+
+/*-----------Coin Slider Captions-----------*/
+
+.slideshow {
+    color: #e00d14;
+}
+
+
+/*-----------Blog-----------*/
+
+.blogpost h5 a {
+    color: #e00d14;
+}
+
+
+/*-----------Top Button-----------*/
+
+.topbutton {
+    text-shadow: #000 0px 1px 0px;
+    color: #fff;
+    background-color: #80060a;
+}
